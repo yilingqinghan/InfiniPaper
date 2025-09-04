a875901ec8dacccb9dab1c15b3db6b02fa481fbd
yilingqinghan <759569457@qq.com>
2025-09-04 12:39:54 +0800

    [Fix][Toast][UI] 修复Toast 超出页面的问题
    Signed-off-by: yilingqinghan <759569457@qq.com>

--- frontend/components/reader/ReaderView.tsx
+++ frontend/components/reader/ReaderView.tsx
@@ -2156,7 +2156,7 @@ React.useEffect(() => { suppressSaveRef.current = true; }, [editorKey, editMode]
               )}
               {/* 左列覆盖内容区（编辑器） */}
               <div className="flex-1 min-h-0 flex flex-col">
-                <div className="min-w-0 min-h-0 flex-1 overflow-auto">
+                <div className="min-w-0 min-h-0 flex-1 overflow-hidden">
                   {editMode === "toast" ? (
                     <div className="h-full" ref={toastRootRef}>
                       <TuiEditor
