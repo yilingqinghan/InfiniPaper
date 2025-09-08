6db98e467f360f00ca9dfca245af44ae442158f0
yilingqinghan <759569457@qq.com>
2025-09-08 08:32:53 +0800

    [Idx] 添加图标和网页名称
    Signed-off-by: yilingqinghan <759569457@qq.com>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,6 +7,7 @@ import { body, display } from "@/lib/fonts";
 import NProgress from "nprogress";
 import Router from "next/router";
 import "nprogress/nprogress.css";
+import Head from "next/head";
 
 // 页面级进度条
 NProgress.configure({ showSpinner: false, trickleSpeed: 80 });
@@ -17,6 +18,13 @@ Router.events.on("routeChangeError", () => NProgress.done());
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>ℑ𝔫𝔣𝔦𝔫𝔦𝔓𝔞𝔭𝔢𝔯</title>
+        <meta name="application-name" content="ℑ𝔫𝔣𝔦𝔫𝔦𝔓𝔞𝔭𝔢𝔯" />
+        <meta name="apple-mobile-web-app-title" content="ℑ𝔫𝔣𝔦𝔫𝔦𝔓𝔞𝔭𝔢𝔯" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+      </Head>
       <div className={`${body.variable} ${display.variable} font-sans`}>
         <TopNav />
         <Component {...pageProps} />
